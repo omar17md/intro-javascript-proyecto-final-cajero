f2e5eee36a3eaccac448c68639db1e23627bfb24
Jesus Omar Magana Medina <jesus.magana@coppel.com>
2022-07-25 20:13:53 -0600

    Se avanzo con el diseño y funcionalidad del cajero

---
+++ js/cajero.js
@@ -0,0 +1,306 @@
+/*
+    ************** Declaración de variables
+*/
+const renglon1 = document.querySelector('#renglon1');
+const renglon2 = document.querySelector('#renglon2');
+const columnas = [
+    document.querySelector('#columna1-1'),
+    document.querySelector('#columna2-1'),
+    document.querySelector('#columna1-2'),
+    document.querySelector('#columna2-2'),
+    document.querySelector('#columna1-3'),
+    document.querySelector('#columna2-3'),
+    document.querySelector('#columna1-4'),
+    document.querySelector('#columna2-4')
+];
+let IDCuenta = -1;
+let procedimiento = 0;
+let numeroOperacion = 0;
+let intentos = 3;
+let password = '';
+let monto = '';
+
+/*
+    ************** Eventos
+*/
+document.addEventListener('DOMContentLoaded', () => {
+   
+});
+
+/*
+    ************** Funciones
+*/
+function InsetarTarjeta(){
+    renglon2.textContent = 'Seleccione una cuenta para comenzar';
+    ConsultarCuentas();
+    procedimiento++;
+}
+
+
+function ConsultarCuentas(){
+    let i = 0;
+    cuentas.forEach(cuenta  => {
+        columnas[i].textContent = cuenta.nombre;
+        i++;
+    });
+};
+
+function SeleccionoOpcion(opcion){
+    switch(procedimiento){
+        case 0:{
+            if(opcion == 1){
+                IniciarPantalla();
+            }
+        }break;
+
+        case 1:{
+            IDCuenta = opcion;
+            LimpiarOpciones();
+            renglon1.textContent = `Ingrese su NIP por favor`;
+            renglon2.innerHTML = '&nbsp;';
+            procedimiento++;
+        }break;
+
+        case 2:{
+            if(password.length == 4){
+                ValidarNIP(password);
+            }
+        }break;
+
+        case 3:{
+            RealizaOperacion(opcion);
+        }break;
+
+        case 4:{
+            if(opcion == 7){
+                CargarPantallaInicio();
+                monto = '';
+                procedimiento = 3;
+            }else if(opcion == 6){
+                IniciarPantalla();
+            }
+        }break;
+
+        case 5:{
+            if(opcion == 3){
+                monto = '';
+                renglon2.textContent = `$ 0`; 
+            }
+            else if(opcion == 5){
+                monto = monto.substring(0, monto.length - 1);
+                if(monto.length == 0){
+                    renglon2.textContent = `$ 0`; 
+                }else{
+                    renglon2.textContent = `$ ${monto}`; 
+                }
+            }else if(opcion == 6){
+                monto = '';
+                CargarPantallaInicio();
+                procedimiento = 3;
+            }else if(opcion == 7){
+                if(numeroOperacion ==1 ){
+                    ValidarIngreso();
+                }else{
+                    ValidaEgreso();
+                }
+            }
+        }break;
+
+        case 6:{
+            if(opcion==6){
+                monto = '';
+                CargarPantallaInicio();
+                procedimiento = 3;
+            }else if(opcion==7){
+                monto = '';
+                LimpiarOpciones();
+                RealizaOperacion(1);
+            }
+        }break;
+    }
+};
+
+function SeleccionoNumero(numero){
+    switch(procedimiento){
+        case 2:{
+            if(password.length < 4){
+                password += numero;
+                renglon2.textContent += '*';
+                if(password.length == 4){
+                    columnas[7].textContent = 'Continuar';
+                }
+            }
+        }break;
+
+        case 5:{
+            monto += numero;
+            renglon2.textContent = `$ ${monto}`;
+        }break;
+    }
+}
+
+function IniciarPantalla(){
+    password = '';
+    procedimiento = 0;
+    intentos = 3;
+    IDCuenta = -1;
+    monto = '';
+    numeroOperacion = 0;
+    renglon1.textContent = 'Bienvenido al Cajero Automatico JS';
+    renglon2.textContent = 'Inserte su tarjeta para comenzar';
+    renglon1.style.color = 'black';
+
+    LimpiarOpciones();
+}
+
+function LimpiarOpciones(){
+    columnas.forEach(columna =>{
+        columna.innerHTML = '&nbsp;';
+        columna.style.color = 'black';
+    });
+}
+
+function ValidarNIP(NIP){
+    if(cuentas[IDCuenta].NIP == NIP){
+        procedimiento++;
+        password = '';
+        CargarPantallaInicio();
+    }else{
+        if(intentos-1 == -1){
+            IniciarPantalla();
+            renglon1.textContent = 'Número de intentos superados';
+            renglon2.textContent = 'Intente más tarde o otra cuenta';
+            renglon1.style.color = 'red';
+
+            columnas[1].textContent = 'Continuar';
+            columnas[1].style.color = 'black';
+
+            procedimiento = 0;
+        }else{
+            columnas[0].textContent = 'NIP Incorrrecto intente de nuevo';
+            columnas[0].style.color = 'red';
+            columnas[1].textContent = `Numero de intentos restante: ${intentos}`;
+            columnas[1].style.color = 'red';
+            columnas[7].innerHTML = '&nbsp;';
+            intentos--;
+
+            renglon2.innerHTML = '&nbsp;';
+            password = '';
+        }
+    }
+}
+
+function CargarPantallaInicio(){
+    LimpiarOpciones();
+
+    renglon1.textContent = `Bienvenido ${cuentas[IDCuenta].nombre} con el numero de cuenta ${cuentas[IDCuenta].numeroCuenta}`;
+    renglon1.style.color = 'black';
+    renglon2.textContent = 'Seleccione la operación a realizar'
+    columnas[0].textContent = 'Consultar saldo';
+    columnas[1].textContent = 'Ingresar monto';
+    columnas[2].textContent = 'Retirar Monto';
+    columnas[6].textContent = 'Salir de la Sesión';
+}
+
+function Continuar(){
+    switch(procedimiento){
+        case 2:{            
+            if(password.length == 4){
+                ValidarNIP(password);
+            }
+        }break;
+    }
+}
+
+function RealizaOperacion(operacion){
+    switch(operacion){
+        case 0:{
+            LimpiarOpciones();
+            renglon1.textContent = 'Tu saldo actual es de: ';
+            renglon2.textContent = `$ ${cuentas[IDCuenta].saldo}`;
+            columnas[6].textContent = 'Salir de la Sesión';
+            columnas[7].textContent = 'Realizar otra operación';
+            procedimiento = 4;
+        }break;
+
+        case 1:{
+            LimpiarOpciones();
+            renglon1.textContent = 'Tecleé el monto a ingresar: ';
+            renglon1.style.color = 'black';
+            renglon2.textContent = '$ 0';
+            columnas[3].textContent = 'Borrar Todo';
+            columnas[5].textContent = 'Borrar';
+            columnas[6].textContent = 'Cancelar';
+            columnas[6].style.color = 'red';
+            columnas[7].textContent = 'Confirmar';
+            columnas[7].style.color = 'green';
+            procedimiento = 5;
+            numeroOperacion = 1;
+        }break;
+
+        case 2:{
+            LimpiarOpciones();
+            renglon1.textContent = 'Tecleé el monto a retirar';
+            renglon1.style.color = 'black';
+            renglon2.textContent = '$ 0';
+            columnas[3].textContent = 'Borrar Todo';
+            columnas[5].textContent = 'Borrar';
+            columnas[6].textContent = 'Cancelar';
+            columnas[6].style.color = 'red';
+            columnas[7].textContent = 'Confirmar';
+            columnas[7].style.color = 'green';
+            procedimiento = 5;
+            numeroOperacion = 2;
+        }break;
+
+        case 6:{
+            IniciarPantalla();
+        }break;
+    }
+}
+
+function ValidarIngreso(){
+    if(Number(monto) + cuentas[IDCuenta].saldo > 990){
+        LimpiarOpciones();
+        renglon1.textContent = 'El monto ingresado más su saldo actual supera los $990';
+        renglon1.style.color = 'red';
+        renglon2.innerHTML = '&nbsp;';
+        columnas[6].textContent = 'Volver al inicio';
+        columnas[7].textContent = 'Ingresar otro monto';
+        procedimiento = 6;
+    }else if(monto == ''){
+        columnas[0].textContent = 'Ingrese un Numero Mayor a 0';
+        columnas[0].style.color = 'red';
+    }else{
+        cuentas[IDCuenta].saldo += Number(monto);
+        LimpiarOpciones();
+        renglon1.textContent = 'Tú nuevo saldo es: ';
+        renglon2.textContent = `$ ${cuentas[IDCuenta].saldo}`;
+        columnas[6].textContent = 'Salir de la Sesión';
+        columnas[7].textContent = 'Realizar otra operación';
+        procedimiento = 4;
+    }
+};
+
+function ValidaEgreso(){
+    if(cuentas[IDCuenta].saldo - Number(monto) < 10){
+        LimpiarOpciones();
+        renglon1.textContent = 'El monto ingresado menos su saldo actual no supera los $10';
+        renglon1.style.color = 'red';
+        renglon2.innerHTML = '&nbsp;';
+        columnas[6].textContent = 'Volver al inicio';
+        columnas[7].textContent = 'Ingresar otro monto a retirar';
+        procedimiento = 6;
+    }else if(monto == ''){
+        columnas[0].textContent = 'Ingrese un Numero Mayor a 0';
+        columnas[0].style.color = 'red';
+    }else{
+        cuentas[IDCuenta].saldo -= Number(monto);
+        LimpiarOpciones();
+        renglon1.textContent = 'Tú nuevo saldo es: ';
+        renglon2.textContent = `$ ${cuentas[IDCuenta].saldo}`;
+        columnas[6].textContent = 'Salir de la Sesión';
+        columnas[7].textContent = 'Realizar otra operación';
+        procedimiento = 4;
+    }
+}
